d1eb775b35fd023c82952b7a2ce37b388dd688cc
steffenbk <43586902+steffenbk@users.noreply.github.com>
2019-12-03 23:39:22 +0100

    Add files via upload

---
+++ Code by Joel/Gun_Inclination.ino
@@ -0,0 +1,73 @@
+// Gun Elevation Code
+// Author: Joel Soto Escoda
+// Autum 2019
+// The following code is used in the angle setting for the gun platform. With the code we can control with one joystick the 
+// motor turn and speed to set the shotting angle.
+
+// Include stepper motor library
+#include <Stepper.h>
+ 
+// Define number of steps per revolution
+#define STEPS 32
+ 
+// Define stepper motor control pins
+#define IN1  8
+#define IN2  9
+#define IN3  10
+#define IN4  11
+ 
+// Initialize stepper library
+Stepper stepper(STEPS, IN4, IN2, IN3, IN1);
+ 
+// Joystick pot output is connected to Arduino A0
+#define joystick  A0
+ 
+void setup()
+{ 
+}
+ 
+void loop()
+{
+  // Read analog value from the potentiometer
+  int val = analogRead(joystick);
+ 
+  // If the joystic is in the middle stop the motor
+  if(  (val > 500) && (val < 523) )
+  {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  }
+ 
+  else
+  {
+    // Move the motor in the first direction
+    while (val >= 523)
+    {
+      // Map the speed between 5 and 500 rpm
+      int speed_ = map(val, 523, 1023, 5, 500);
+      // Set motor speed
+      stepper.setSpeed(speed_);
+ 
+      // Move the motor (1 step)
+      stepper.step(1);
+ 
+      val = analogRead(joystick);
+    }
+ 
+    // Move the motor in the other direction
+    while (val <= 500)
+    {
+      // Map the speed between 5 and 500 rpm
+      int speed_ = map(val, 500, 0, 5, 500);
+      // Set motor speed
+      stepper.setSpeed(speed_);
+ 
+      // Move the motor (1 step)
+      stepper.step(-1);
+ 
+      val = analogRead(joystick);
+    }
+   }
+ }
---
+++ Code by Joel/Motors_and_Joystick.ino
@@ -0,0 +1,104 @@
+// Manual driving Code
+// Author: Joel Soto Escoda
+// Autum 2019
+// The following code is used in the manual driving code. We're going to use a potentiometer 
+// which we're going to control both motors and the directions.
+
+// Motor A pin control
+#define enA 2 // Control of the velocity
+#define in1 50 // Control of the direction
+#define in2 51 // Control of the direction
+
+// Motor B pin control
+#define enB 3 // Control of the velocity
+#define in3 52 // Control of the direction
+#define in4 53 // Control of the direction
+
+int motorSpeedA = 0;
+int motorSpeedB = 0;
+
+void setup() {
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+}
+
+void loop() {
+  int xAxis = analogRead(A0); // Read Joysticks X-axis
+  int yAxis = analogRead(A1); // Read Joysticks Y-axis
+  
+  // Going backward
+  if (yAxis < 470) {
+    // Set Motor A backward
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    // Set Motor B backward
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    // Convert the declining Y-axis readings for going backward from 470 to 0 into 0 to 255 value for the PWM signal for increasing the motor speed
+    motorSpeedA = map(yAxis, 470, 0, 0, 255);
+    motorSpeedB = map(yAxis, 470, 0, 0, 255);
+  }
+  // Going forward
+  else if (yAxis > 550) {
+    // Set Motor A forward
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    // Set Motor B forward
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH);
+    // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
+    motorSpeedA = map(yAxis, 550, 1023, 0, 255);
+    motorSpeedB = map(yAxis, 550, 1023, 0, 255);
+  }
+
+  // Stop
+  else {
+    // Set Motor A stop
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    // Set Motor B stop
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, LOW);
+  }
+  // Going RIGHT
+  if (xAxis < 470) {
+   // Set Motor A forward
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    // Set Motor B backward
+    digitalWrite(in3, HIGH);
+    digitalWrite(in4, LOW);
+    // Convert the declining X-axis readings from 470 to 0 into increasing 0 to 255 value
+    motorSpeedA = map(yAxis, 470, 0, 0, 255);
+    motorSpeedB = map(yAxis, 470, 0, 0, 255);
+  }
+
+  // Going LEFT
+  if (xAxis > 550) {
+    // Set Motor A backward
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    // Set Motor B forward
+    digitalWrite(in3, LOW);
+    digitalWrite(in4, HIGH); 
+    // Convert the increasing X-axis readings from 550 to 1023 into 0 to 255 value
+    motorSpeedA = map(yAxis, 550, 1023, 0, 255);
+    motorSpeedB = map(yAxis, 550, 1023, 0, 255);   
+  }
+  
+  // Prevent buzzing at low speeds
+  if (motorSpeedA < 70) {
+    motorSpeedA = 0;
+  }
+  if (motorSpeedB < 70) {
+    motorSpeedB = 0;
+  }
+  // Send the PWN signal to the motor
+  analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
+  analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
+ }
